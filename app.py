1f97a1d7d18ae87e9d0482dc0210ce59d7779347
sethukarthikeyan <58760679+sethukarthikeyan@users.noreply.github.com>
2020-04-14 23:38:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,51 +8,44 @@ from bokeh.embed import components
 from bokeh.plotting import figure
 from bokeh.resources import INLINE
 
-connCreateURL = 'https://pomber.github.io/covid19/timeseries.json'
-
 masterlist = []
-response = requests.get(url=connCreateURL)
-projjsonData = json.loads(str(response.text))
-for row in projjsonData.keys():
-    for itm in projjsonData[row]:
-        masterlist.append([row,itm['date'], itm['confirmed'],itm['deaths'],itm['recovered']])
-        
-data = pd.DataFrame(masterlist, columns=['Country', 'Date', 'Confirmed', 'Deaths','Recovered'])
+url = "https://corona-virus-world-and-india-data.p.rapidapi.com/api"
+
+headers = {
+    'x-rapidapi-host': "corona-virus-world-and-india-data.p.rapidapi.com",
+    'x-rapidapi-key': "eb025b1b84msh5de681705d5ee71p1aa9b6jsn964a2b2c0df3"
+    }
+
+response = requests.request("GET", url, headers=headers)
+
+data = json.loads(response.text)
+refreshed_Date = data['statistic_taken_at'].split()[0]
+for itm in data['countries_stat']:
+    masterlist.append([itm['country_name'],refreshed_Date, itm['cases'],itm['new_cases'],itm['deaths'],itm['new_deaths'],itm['total_recovered'],itm['active_cases'],itm['serious_critical'],itm['total_cases_per_1m_population']])
+       
+data = pd.DataFrame(masterlist, columns=['Country', 'Date', 'Total Cases', 'New Cases','Total Deaths','New Deaths', 'Total Recovered','Active Cases','Serious Critical','Total Cases/1M Population'])
 data.set_index(['Country'], inplace=True)
 data.sort_index()
+data.fillna(0) 
+data = data.replace('N/A',0)
 data.index.name=None
 data["Date"] = pd.to_datetime(data["Date"])
-#data.sort_values(by='Date', inplace=True, ascending=False)
-yesterday = datetime.strftime(datetime.now() - timedelta(2), '%Y-%m-%d')
-curr_date = datetime.strftime(datetime.now() - timedelta(1), '%Y-%m-%d')
 
-top_df = data.loc[data['Date']==curr_date]
-last_df = data.loc[data['Date']==yesterday]
+print(data['Total Recovered'])
 
-confirmed_diff = []
-deaths_diff = []
-recovered_diff = []
+for column in ['Total Cases', 'New Cases','Total Deaths','New Deaths','Total Recovered', 'Active Cases','Serious Critical','Total Cases/1M Population']:
+    print(column)
+    data[column] = data[column].str.replace(',', '').astype(float)
 
-for itm in top_df.index.values:
-    confirmed_diff.append(top_df.loc[itm]['Confirmed'] - last_df.loc[itm]['Confirmed'])
-    deaths_diff.append(top_df.loc[itm]['Deaths'] - last_df.loc[itm]['Deaths'])
-    recovered_diff.append(top_df.loc[itm]['Recovered'] - last_df.loc[itm]['Recovered'])
-
-top_df = top_df.assign(New_Cases = confirmed_diff)
-top_df = top_df.assign(New_Deaths = deaths_diff)
-top_df = top_df.assign(New_Recovered = recovered_diff)
-
-top_df = top_df.rename(columns = {"New_Cases": "New Cases", 
-                                  "New_Deaths":"New Deaths", 
-                                  "New_Recovered": "New Recovered"}) 
+pd.options.display.float_format = '{:,.0f}'.format
 
 # First Chart
-top_df1 = top_df.sort_values(by='Confirmed', ascending=False)
+top_df1 = data.sort_values(by='Total Cases', ascending=False)
 my_df1 = top_df1.head(10)
 countries1 = list(my_df1.index)
-counts1 = list(my_df1['Confirmed'])
+counts1 = list(my_df1['Total Cases'])
 TOOLTIPS = [
-    ("Confirmed", "@top")
+    ("Total Cases", "@top")
 ]
 p = figure(x_range=countries1, plot_height=250,tooltips=TOOLTIPS, title="Top 10 Countries by Confirmed Cases",
             toolbar_location=None, tools="")
@@ -67,12 +60,12 @@ script, div = components(p)
 
 
 #second chart
-top_df2 = top_df.sort_values(by='Deaths', ascending=False)
+top_df2 = data.sort_values(by='Total Deaths', ascending=False)
 my_df2 = top_df2.head(10)
 countries2 = list(my_df2.index)
-counts2 = list(my_df2['Deaths'])
+counts2 = list(my_df2['Total Deaths'])
 TOOLTIPS = [
-    ("Deaths", "@top")
+    ("Total Deaths", "@top")
 ]
 p2 = figure(x_range=countries2, plot_height=250,tooltips=TOOLTIPS, title="Top 10 Countries by Death Cases",
             toolbar_location=None, tools="")
@@ -87,12 +80,12 @@ script2, div2 = components(p2)
 
 
 #third chart
-top_df3 = top_df.sort_values(by='Recovered', ascending=False)
+top_df3 = data.sort_values(by='Total Recovered', ascending=False)
 my_df3 = top_df3.head(10)
 countries3 = list(my_df3.index)
-counts3 = list(my_df3['Recovered'])
+counts3 = list(my_df3['Total Recovered'])
 TOOLTIPS = [
-    ("Recovered", "@top")
+    ("Total Recovered", "@top")
 ]
 p3 = figure(x_range=countries3, plot_height=250,tooltips=TOOLTIPS, title="Top 10 Countries by Recovered Cases",
             toolbar_location=None, tools="")
@@ -107,9 +100,27 @@ script3, div3 = components(p3)
 
 app = Flask(__name__)
 
+#<table id="dtBasicExample" class="table table-striped table-bordered table-sm" cellspacing="0" width="100%">
+data.sort_index()
+final_HTML = re.sub(' mytable', '" id="mytable', data.to_html(classes='mytable'))
+final_HTML = re.sub('class="dataframe"','class="js-sort-table" ', final_HTML)
+
+changesDict = {'<th>Date</th>': '<th class="js-sort-date">Date</th>',
+'<th>Total Cases</th>': '<th class="js-sort-number">Total Cases</th>',
+'<th>New Cases</th>':'<th class="js-sort-number">New Cases</th>',
+'<th>Total Deaths</th>':'<th class="js-sort-number">Total Deaths</th>',
+'<th>New Deaths</th>':'<th class="js-sort-number">New Deaths</th>',
+'<th>Total Recovered</th>':'<th class="js-sort-number">Total Recovered</th>',
+'<th>Active Cases</th>':'<th class="js-sort-number">Active Cases</th>',
+'<th>Total Cases/1M Population</th>':'<th class="js-sort-number">Total Cases/1M Population</th>',
+'<th>Serious Critical</th>':'<th class="js-sort-number">Serious Critical</th>'}
+
+for change in changesDict.keys():
+    final_HTML = re.sub(change,changesDict[change], final_HTML)
+    
 @app.route('/')
 def home():
-    return render_template('index.html',tables=[re.sub(' mytable', '" id="mytable', top_df.loc[top_df['Date']==curr_date].to_html(classes='mytable'))], titles = ['na'],
+    return render_template('index.html',tables=[final_HTML], titles = ['na'],
     plot_script=script,
     plot_div=div,
     plot_script2=script2,
@@ -118,7 +129,6 @@ def home():
     plot_div3=div3,
     js_resources=js_resources,
     css_resources=css_resources)
-    #    return render_template('index.html',tables=[data.loc[data['Date']==yesterday].to_html()], titles = ['na', 'Countrywise'])
 
 if __name__ == "__main__":
     app.run(debug=True)
